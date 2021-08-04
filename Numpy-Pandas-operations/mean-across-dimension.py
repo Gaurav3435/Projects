031c6dd6ab68866812a88f730ab1b952c629adf9
Gaurav3435 <gauravpatil22301@gmail.com>
2021-08-04 10:15:59 +0530

    Modified FIle

--- Numpy-Pandas-operations/mean-across-dimension.py
+++ Numpy-Pandas-operations/mean-across-dimension.py
@@ -1,4 +1,3 @@
-
 # Importing Library
 import numpy as np
   
@@ -7,25 +6,18 @@ arr = np.array([[10, 20, 30], [40, 55, 66], [70, 80, 90]])
   
 # Calculating mean across Rows
 row_mean = np.mean(arr, axis=1)
-  
 row1_mean = row_mean[0]
-print("Mean of Row 1 is", row1_mean)
-  
+print("Mean of Row 1 is", row1_mean) 
 row2_mean = row_mean[1]
 print("Mean of Row 2 is", row2_mean)
-  
 row3_mean = row_mean[2]
 print("Mean of Row 3 is", row3_mean)
   
-  
 # Calculating mean across Columns
 column_mean = np.mean(arr, axis=0)
-  
 column1_mean = column_mean[0]
 print("Mean of column 1 is", column1_mean)
-  
 column2_mean = column_mean[1]
 print("Mean of column 2 is", column2_mean)
-  
 column3_mean = column_mean[2]
 print("Mean of column 3 is", column3_mean)
